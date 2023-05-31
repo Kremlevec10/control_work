73191846b4ffca74a377b7f5a39ddfc463f2efd8
Ruslan <Ruslan90.0843@gmail.com>
2023-05-31 18:58:59 +0300

    initial commit

---
+++ model/Notes.py
@@ -0,0 +1,10 @@
+class Notes:
+    def __init__(self):
+        self.id = 0
+        self.list_notes = []
+
+    def add_note(self,note):
+        self.list_notes.append(note)
+
+    def show_all_notes(self):
+        pass
---
+++ model/task.py
@@ -0,0 +1,16 @@
+class Task:
+    def __init__(self):
+        self.id
+        self.title
+        self.text
+        self.date
+
+
+    def create(self,):
+        self.id = Task.id
+
+    def save(self):
+        pass
+
+    def edit(self):
+        pass
